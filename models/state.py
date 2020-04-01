2f0a831e3cc0da32dd7d04c6322150cf072e5797
paurbano <paurbano@gmail.com>
2020-04-01 21:07:42 +0000

    Modify class State for task 6

--- models/state.py
+++ models/state.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
 """This is the state class"""
+import models
+from os import getenv
 from models.base_model import BaseModel
+from models.city import City, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column,  Integer, String
+from sqlalchemy.orm import relationship
+
+# Added for task 6
+# Base = declarative_base()
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    # Added for task 6
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade="all, delete")
+    else:
+        name = ""
+
+    # Added for task 6
+    if getenv("HBNB_TYPE_STORAGE") == "file":
+        @property
+        def cities(self):
+            list_of_cities = []
+            dic_cities = models.Storage.all(City)
+            for city in dic_cities.items():
+                if city.state_id == self.id:
+                    list_of_cities.append(city)
+            return list_of_cities
