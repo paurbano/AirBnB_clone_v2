66fd0c3388d7f85c71d3dab82bb4b530eea662b8
paurbano <paurbano@gmail.com>
2020-04-02 00:54:08 +0000

    fix conditons for task 8

--- models/place.py
+++ models/place.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This is the place class"""
 from os import getenv
-from models.base_model import BaseModel
-from models.review import Review, Base
-from sqlalchemy import Column,  Integer, String, ForeignKey, Float
+from models.base_model import BaseModel, Base
+from models.review import Review
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 
 
@@ -22,22 +22,35 @@ class Place(BaseModel, Base):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
     # Condition required for task 9
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "places"
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
         reviews = relationship('Review', backref='place',
                                cascade='all, delete')
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = ""
+        number_bathrooms = ""
+        max_guest = ""
+        price_by_night = ""
+        latitude = ""
+        longitude = ""
+        amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def reviews(self):
             list_reviews = []
