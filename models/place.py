ce067e88af50b6a4f05a4a09010885a55fa95e60
leocjj <leocj@hotmail.com>
2020-04-02 11:53:28 -0500

    Task 10 update

--- models/place.py
+++ models/place.py
@@ -15,18 +15,7 @@ place_amenity = Table("place_amenity", Base.metadata,
                              nullable=False),
                       Column('amenity_id', String(60),
                              ForeignKey('amenities.id'), primary_key=True,
-                             nullable=False)
-                      )
-
-'''# Requierement  for task 10
-place_amenity = Table("place_amenity", Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'), nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'), nullable=False)
-                      )
-'''
-
+                             nullable=False))
 
 class Place(BaseModel, Base):
     """This is the class for Place
