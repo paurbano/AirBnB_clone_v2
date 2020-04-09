43e177bca9d478c6e142065aefa18ba5ff662ed5
paurbano <paurbano@gmail.com>
2020-04-09 20:45:19 +0000

    script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,13 +3,13 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 ALIAS="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
-SRC="/etc/nginx/sites-enable/default"
+SRC="/etc/nginx/sites-enabled/default"
 sudo mkdir /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "<html><head><\head><body>Hello World from:...<\body><\html>" > /data/web_static/releases/test/index.html
+echo "Hello World from:..." > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "30i $ALIAS" $SRC
