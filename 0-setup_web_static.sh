52b7378b200ea520970f3cdc78b46f671f834ddb
paurbano <paurbano@gmail.com>
2020-04-09 19:47:50 +0000

    script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,20 +2,12 @@
 # sets up web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-ALIAS="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-SRC="/etc/nginx/sites-enable/default"
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
-echo "<html><head><\head><body>Hello World from:..<\body><\html>" > /data/web_static/releases/test/index.html
-if [ -L "/data/web_static/current" ]
-then
-    sudo rm "/data/web_static/current"
-else
-    sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-fi
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "30i $ALIAS" $SRC
+SRC="/etc/nginx/sites-available/default"
+STATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static_current/;\n\t}\n"
+sudo mkdir -p "/data/web_static/releases/test/"
+sudo mkdir "/data/web_static/shared/"
+echo "Hello World" | sudo tee "/data/web_static/releases/test/index.html"
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -R ubuntu:ubuntu "/data/"
+sudo sed -i "30i $STATIC" $SRC
 sudo service nginx restart
