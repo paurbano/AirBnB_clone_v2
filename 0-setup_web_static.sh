d97f368e5c94f45f5aa99708d9a11365ca0ea53f
paurbano <paurbano@gmail.com>
2020-04-09 19:37:22 +0000

    script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 # sets up web servers for the deployment of web_static
-ALIAS="\\\tlocation /hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}\n"
+sudo apt-get -y update
+sudo apt-get -y install nginx
+ALIAS="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 SRC="/etc/nginx/sites-enable/default"
 sudo mkdir /data/
 sudo mkdir /data/web_static/
 sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
-echo "<html><head><\head><body> Holberton School<\body><\html>" > /data/web_static/releases/test/index.html
+echo "<html><head><\head><body>Hello World from:..<\body><\html>" > /data/web_static/releases/test/index.html
 if [ -L "/data/web_static/current" ]
 then
-    ubuntu rm "/data/web_static/current"
+    sudo rm "/data/web_static/current"
 else
     sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 fi
