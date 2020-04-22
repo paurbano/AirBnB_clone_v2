841f6b9a86ee2ef88831c922f06635f5892d94f7
paurbano <paurbano@gmail.com>
2020-04-22 21:06:05 +0000

    List states quering DB, use of template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
@@ -10,7 +11,12 @@ app = Flask(__name__)
 @app.route('/states_list',  strict_slashes=False)
 def display_states_list():
     """ retrieve list of states and passes to template"""
-    states_list = storage.all("State")
+    # states_list = storage.all("State")
+    states_list = []
+    dic_states = storage.all(State)
+    for state in dic_states.values():
+        # for city in dic_cities.values():
+        states_list.append(state)
     return render_template('7-states_list.html', states=states_list)
 
 
